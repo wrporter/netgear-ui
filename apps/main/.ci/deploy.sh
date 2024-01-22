5d88b0dc42ed830924be538c0e9cb1a9d4a4a3aa
wrporter <wesleyrporter@gmail.com>
2024-01-22 12:13:36 -0700

    Try rsync from GitHub Actions

--- apps/main/.ci/deploy.sh
+++ apps/main/.ci/deploy.sh
@@ -7,6 +7,8 @@ source .ci/config.sh
 
 # Inject secrets
 injectSecrets
+rsync -azPI bin/.env.prod ${SSH_USER}@${SSH_HOST}:${BASE_DIRECTORY}/${APP}/
+echo "-- Successfully rsync-ed file."
 scp -O ${SCP_PORT_OPT} .env.prod ${SSH_USER}@${SSH_HOST}:${BASE_DIRECTORY}/${APP}/.env
 
 # Deploy updated docker-compose config
